9bc16ba00d235f1306d311bd9d2ac9a86ef26144
Michela Faella <108745604+MichelaFaella@users.noreply.github.com>
2023-01-07 11:04:54 +0100

    aggiornamento header
    @AleB01

--- Front/Html/generate-cod/random.js
+++ Front/Html/generate-cod/random.js
@@ -2,7 +2,7 @@
 function getCode(){
 
     var chars ="0123456789abcdefghijklmnopqrstuvwxtzABCDEFGHIJKLMNOPQRSTUVWTZ";
-    var codeLenght = 16;
+    var codeLenght = 8;
     var code = "";
 
     for (var i=0;i<codeLenght; i++){
