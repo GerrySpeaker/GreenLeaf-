6aa1f38b631c00e50b49b33102f95267aa464ad4
Michela Faella <108745604+MichelaFaella@users.noreply.github.com>
2023-01-07 15:46:23 +0100

    Generazione codice regalo
    @AleB01

--- Presentation/Html/generate-cod/random.js
+++ Presentation/Html/generate-cod/random.js
@@ -4,10 +4,29 @@ function getCode(){
     var chars ="0123456789abcdefghijklmnopqrstuvwxtzABCDEFGHIJKLMNOPQRSTUVWTZ";
     var codeLenght = 8;
     var code = "";
+    var btn = document.getElementById("btn");
+    var btncopy = document.getElementById("btn-copy");
 
     for (var i=0;i<codeLenght; i++){
         var randomNumber = Math.floor(Math.random() * chars.length);
         code += chars.substring(randomNumber,randomNumber+1);
     }
     document.getElementById("code").value = code
+    btn.style.display = "none";
+    btncopy.style.display = "block";
 }
+
+function copy() {
+    // Get the text field
+    var copyText = document.getElementById("code");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Ho copiato il seguente codice regalo: " + copyText.value);
+  }
