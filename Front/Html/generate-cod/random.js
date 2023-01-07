4a3a93937f64a4f9225bceffb1a8fb5692489dca
AleB01 <alexborrelli2001@gmail.com>
2023-01-07 15:47:07 +0100

    footer

---
+++ Front/Html/generate-cod/random.js
@@ -0,0 +1,13 @@
+
+function getCode(){
+
+    var chars ="0123456789abcdefghijklmnopqrstuvwxtzABCDEFGHIJKLMNOPQRSTUVWTZ";
+    var codeLenght = 8;
+    var code = "";
+
+    for (var i=0;i<codeLenght; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        code += chars.substring(randomNumber,randomNumber+1);
+    }
+    document.getElementById("code").value = code
+}
