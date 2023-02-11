26a33a9ac340427b7ff4b9eee0143a1bf1834fbd
Vitale01 <114576360+Vitale01@users.noreply.github.com>
2023-02-11 19:33:58 +0100

    inserimento JavaDoc
    Co-Authored-By: vincenzo0203 <114588862+vincenzo0203@users.noreply.github.com>
    Co-Authored-By: GerrySpeaker <105554339+GerrySpeaker@users.noreply.github.com>
    Co-Authored-By: AleB01 <105659160+AleB01@users.noreply.github.com>
    Co-Authored-By: Michela Faella <108745604+MichelaFaella@users.noreply.github.com>

---
+++ javaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"application.autenticazione"},{"l":"application.gestionealberiadottati"},{"l":"application.gestionealberidaadottare"},{"l":"application.gestioneregalo"},{"l":"application.gestionipiantumazioni"},{"l":"application.registrazione"},{"l":"bean"},{"l":"storage"}];updateSearchResults();
