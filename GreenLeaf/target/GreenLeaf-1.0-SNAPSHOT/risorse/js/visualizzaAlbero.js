a1e485880e3eff2aa4c9804aa5410a767959e5c0
vincenzo0203 <v.cerciello1@studenti.unisa.it>
2023-02-13 18:33:07 +0100

    Reload

--- GreenLeaf/target/GreenLeaf-1.0-SNAPSHOT/risorse/js/visualizzaAlbero.js
+++
@@ -1,7 +0,0 @@
-function visualizzaAlbero(id) {
-    $.ajax({ //GET categorie
-        type: 'GET', //tipo POST, GET
-        url: 'AlberiAdottati', //metto la servlet
-        data: {idAlbero: id}, //metto i campi che devo mandare tramite la richiesta
-    });
-}
